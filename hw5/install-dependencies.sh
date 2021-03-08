f303b88f0805948b297678ecd79f1deb12a54201
Zach Zaiman <zzaiman@emory.edu>
2021-03-08 09:08:40 -0500

    homework5 not finished

---
+++ hw5/install-dependencies.sh
@@ -0,0 +1,18 @@
+#Clone and make the darknet repository
+
+echo "Cloning GitHub Repo and downloading required model weights"
+
+git clone https://github.com/AlexeyAB/darknet.git
+cd darknet
+make
+
+wget https://pjreddie.com/media/files/yolov2-voc.weights
+wget https://pjreddie.com/media/files/yolov2-tiny.weights
+
+echo "Running inference and saving as a textfile"
+
+./darknet detector test cfg/voc.data cfg/yolov2-voc.cfg yolov2-voc.weights -dont_show -ext_output < ../valid.txt  > ../standard_results.txt
+
+./darknet detector test cfg/voc.data cfg/yolov2-tiny.cfg yolov2-tiny.weights -dont_show -ext_output < ../valid.txt  > ../tiny_results.txt
+
+echo "Process Completed"
