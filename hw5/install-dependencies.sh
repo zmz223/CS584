4717c30bf2182465d55b8b07ff7ce3a68074a085
Zach Zaiman <zzaiman@emory.edu>
2021-03-08 22:52:54 -0500

    homework 5

--- hw5/install-dependencies.sh
+++ hw5/install-dependencies.sh
@@ -7,12 +7,12 @@ cd darknet
 make
 
 wget https://pjreddie.com/media/files/yolov2-voc.weights
-wget https://pjreddie.com/media/files/yolov2-tiny.weights
+wget https://pjreddie.com/media/files/yolov2-tiny-voc.weights
 
 echo "Running inference and saving as a textfile"
 
 ./darknet detector test cfg/voc.data cfg/yolov2-voc.cfg yolov2-voc.weights -dont_show -ext_output < ../valid.txt  > ../standard_results.txt
 
-./darknet detector test cfg/voc.data cfg/yolov2-tiny.cfg yolov2-tiny.weights -dont_show -ext_output < ../valid.txt  > ../tiny_results.txt
+./darknet detector test cfg/voc.data cfg/yolov2-tiny-voc.cfg yolov2-tiny-voc.weights -dont_show -ext_output < ../valid.txt  > ../tiny_results.txt
 
 echo "Process Completed"
